2f40b39e512050a52be97832e176063fc930f0b8
coder.peng <1047871481@qq.com>
2026-01-29 09:17:04 +0800

    Initial commit

---
+++ WeGame/公告.h
@@ -0,0 +1,10 @@
+#pragma once
+#include<string>
+using namespace std;
+
+VOID SetWindowPEdit(CEdit* pEdit);
+CEdit* GetWindowPEdit();
+
+VOID 监控(CString message);
+VOID 神话公告(string message, int type);
+VOID 游戏公告(string message, int type);
